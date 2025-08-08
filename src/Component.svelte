63c00d696dea809097d7cb9575240eeaaa9e1785
ony arianto <onyarianto03@gmail.com>
2025-08-08 19:17:04 +0700

    update pdf

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,7 +1,10 @@
 <script>
   import { getContext } from "svelte"
   import PdfViewer from 'svelte-pdf';
+  import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
+  import workerSrc from 'pdfjs-dist/legacy/build/pdf.worker.entry?worker&inline';
 
+  pdfjsLib.GlobalWorkerOptions.workerPort = new workerSrc();
 
   export let text
 
