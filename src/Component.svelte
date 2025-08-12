aa5dd36824fc3954102e920a9e2e5ea6696a6eeb
ony arianto <onyarianto03@gmail.com>
2025-08-12 17:21:44 +0700

    update pdf

--- src/Component.svelte
+++ src/Component.svelte
@@ -2,8 +2,6 @@
   import { getContext } from "svelte"
   import Pdfviewer from "./pdfviewer.svelte"
 
-  pdfjs.GlobalWorkerOptions.workerSrc = "https://app.unpkg.com/pdfjs-dist@5.4.54/files/build/pdf.worker.mjs";
-
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 
