d03dcfbf91cd1335c0bc70bb00b38f074154ca65
ony arianto <onyarianto03@gmail.com>
2025-08-12 11:10:11 +0700

    create button print

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,24 +1,20 @@
 <script>
   import { getContext } from "svelte"
-  import { onMount } from "svelte";
-  let PdfViewer;
-
-  onMount(async () => {
-    const module = await import("svelte-pdf");
-    PdfViewer = module.default;
-  });
-    
-
-
-  export let text
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 
+  const handleClick = () => {
+    window.print()
+    console.log('handle print')
+    component.handleClick()
+  }
+
 </script>
 
 <div use:styleable={$component.styles}>
-  {#if PdfViewer}
-    <svelte:component this={PdfViewer} url={text} />
-  {/if}
+  <button on:click={handleClick}>
+    {$component.label}
+    <slot />
+  </button>
 </div>
